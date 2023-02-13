64630f76c4b5bf0ba243838206a8e16db756b424
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-02-13 08:10:44 -0800

    added player table

--- public/js/player/carried.js
+++
@@ -1,9 +0,0 @@
-const {Player} = require('../../../models/player');
-const sequelize = require('../../../config/connection');
-
-class Carried extends Player {
-    // cannot exceed 5
-    // if exceeds 5 force player discard down to 5
-}
-
-module.exports = Carried;
--- public/js/player/hands.js
+++
@@ -1,8 +0,0 @@
-const {Player} = require('../../../models/player');
-const sequelize = require('../../../config/connection');
-
-class Hands extends Player {
-    // can not be greater than two, reads play field
-}
-
-module.express = Hands;
