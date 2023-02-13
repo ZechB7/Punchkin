82dd210fc3855cfa3d1e93cb04daf46ad2d61139
Kyle Hipple <76081114+KlyDesign@users.noreply.github.com>
2023-02-13 11:47:48 -0600

    Added playgame route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -79,10 +79,10 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-router.get('/combat', (req, res) => {
+router.get('/playgame', (req, res) => {
   // If the user is already logged in, redirect the request to another route
-  if (req.session.logged_in) {
-    res.redirect('/profile');
+  if (!req.session.logged_in) {
+    res.redirect('/login');
     return;
   }
   res.render('playGame');
