03cd6663f56c1f458dd1d2ad805496c2c81d27b4
Reuben Schmolke <sandwichcrashers@gmail.com>
2023-02-13 08:12:42 -0800

    player items

---
+++ public/js/player/hands.js
@@ -0,0 +1,8 @@
+const {Player} = require('../../../models/player');
+const sequelize = require('../../../config/connection');
+
+class Hands extends Player {
+    // can not be greater than two, reads play field
+}
+
+module.express = Hands;
