ac344c666df329a6ca6265fa657915694b30e532
joshuagoeke <113941639+joshuagoeke@users.noreply.github.com>
2023-02-13 12:13:39 -0600

    synthesizing Reuben's player.js with my InstanceSave.js

--- models/InstanceSave.js
+++ models/InstanceSave.js
@@ -2,76 +2,82 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 const {user} = require('./user')
 
-
 class InstanceSave extends Model {}
 
-// InstanceSave.init(
-//   {
-//     id: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//       primaryKey: true,
-//       autoIncrement: true,
-//     },
-//     // user_name: {
-//     //   type: DataTypes.STRING,
-//     //   allowNull: false,
-//     //   references: {
-//     //     model: 'user',
-//     //     key: 'name',
-//     //   }
-//     // },
-//     level: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     race: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     class: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     attack_bonus: {
-//       type: DataTypes.INTEGER,
-//       allowNull: false,
-//     },
-//     // cards_in_hand: { //may need PostgreSQL
-//     //   type: DataTypes.STRING,
-//     // }, 
-//     headgear: {
-//       type: DataTypes.STRING,
-//     },
-//     armor: {
-//       type: DataTypes.STRING,
-//     },
-//     hand1: {
-//       type: DataTypes.STRING,
-//     },
-//     hand2: {
-//       type: DataTypes.STRING,
-//     },
-//     shoes: {
-//       type: DataTypes.STRING,
-//     },
-//     game_state: {
-//       type: DataTypes.STRING,
-//       allowNull: false,
-//     },
-//     monster: {
-//       type: DataTypes.STRING,
-//     },
-
-   
-//   },
-//   {
-//     sequelize,
-//     timestamps: false,
-//     freezeTableName: true,
-//     underscored: true,
-//     modelName: 'instancesave',
-//   }
-// );
+InstanceSave.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+        },
+        // user_name: {
+        //   type: DataTypes.STRING,
+        //   allowNull: false,
+        //   references: {
+        //     model: 'user',
+        //     key: 'name',
+        //   }
+        // },
+        level: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        buff: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        strength: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        race: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },  
+        class: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        carried: { //actually an array of card id's
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        in_play: { //actually an array of card id's
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        headgear: {
+            type: DataTypes.STRING,
+        },
+        armor: {
+            type: DataTypes.STRING,
+            allowNull: true,
+        },
+        hands: { //actually an array of card id's limit on front end of 2 items
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+        shoes: {
+            type: DataTypes.STRING,
+        },
+        run: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        game_state: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        timestamps: false,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'player',
+    }
+);
 
 module.exports = InstanceSave;
